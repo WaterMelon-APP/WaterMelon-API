4d02e2d1088d8a343e6951e512532975acaf9184
Miran <mirann.her@gmail.com>
2020-01-11 00:57:01 +0100

    Recommit clean de la route User

---
+++ README.md
@@ -0,0 +1,2 @@
+# Notre magnifique API Watermelon.
+# Développée avec .NET Core 3.1
---
+++ WaterMelon-API/Models/User.cs
@@ -0,0 +1,24 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+using Newtonsoft.Json;
+
+namespace WaterMelon_API.Models
+{
+    public class User
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+
+        [BsonElement("Name")]
+        [JsonProperty("Name")]
+        public String Name { get; set; }
+
+        public String Username { get; set; }
+
+        public String Password { get; set; }
+
+        public String Email { get; set; }
+    }
+}
