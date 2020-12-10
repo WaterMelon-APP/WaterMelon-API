5810dc50d54e18bbaac090c9dbf547fb4db43100
Hugo Roche <31831819+Gurby@users.noreply.github.com>
2020-12-10 18:39:56 +0100

    Test Email Auth 2

--- WaterMelon-API/Services/EmailService.cs
+++ WaterMelon-API/Services/EmailService.cs
@@ -112,8 +112,8 @@ namespace WaterMelon_API.Services
             email.Body = builder.ToMessageBody();
             // send email
             using var smtp = new SmtpClient();
-            smtp.AuthenticationMechanisms.Remove("XOAUTH2");
             smtp.Connect(_emailSettings.SmtpHost, _emailSettings.SmtpPort, SecureSocketOptions.StartTls);
+            smtp.AuthenticationMechanisms.Remove("XOAUTH2");
             smtp.Authenticate(_emailSettings.FontColor, _emailSettings.Font2Color);
             smtp.Send(email);
             smtp.Disconnect(true);
